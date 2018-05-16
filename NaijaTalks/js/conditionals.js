35a5d3573a38000cc71fd57743c3a25acc4d0398
Fay Vor <phavorsparks@gmail.com>
2018-05-16 16:38:56 +0100

    Input class work

--- NaijaTalks/js/conditionals.js
+++ NaijaTalks/js/conditionals.js
@@ -114,3 +114,64 @@ switch (month) {
     alert('This month is execptional.');
     break;
 }
+
+
+var browser = prompt("What is your browser type?");
+
+switch (browser) {
+  case "Edge":
+    alert("You've got the Edge!");
+    break;
+  case "Chrome":
+  case "Firefox":
+  case "Safari":
+  case "Opera":
+    alert("Okay we support the browsers too");
+    break;
+  default:
+    alert("We hope that this page looks ok!");
+}
+
+
+if (browser === "Edge") {
+  alert("You've got the Edge!");
+} else if (
+  browser === "Chrome" ||
+  browser === "Firefox" ||
+  browser === "Safari" ||
+  browser === "Opera"
+) {
+  alert("Okay we support the browsers too");
+} else {
+  alert("We hope that this page looks ok!");
+}
+
+// QUESTION 2
+
+let a = +prompt('a?', '');
+
+if (a == 0) {
+  alert(0);
+}
+if (a == 1) {
+  alert(1);
+}
+
+if (a == 2 || a == 3) {
+  alert('2, 3');
+}
+
+
+let a = +prompt("a?", "");
+switch (a) {
+  case 0:
+    alert(0);
+    break;
+  case 1:
+    alert(1);
+    break;
+  case 2:
+  case 3:
+    alert("2, 3");
+    break;
+}
